5f7e59f1a7ea71cbc27186e2262e87acc1707c31
Nick Lowe <nicholas.lowe@ufl.edu>
2024-04-03 21:20:26 -0400

    Update PlayerMovement.cs
    Image of player changes based on direction or if falling. No falling to the left image located, though.

--- the mathing g8tor/Assets/PlayerMovement.cs
+++ the mathing g8tor/Assets/PlayerMovement.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D player;
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] spriteArray;
 
     // Start is called before the first frame update
     private void Start()
     {
         player = GetComponent<Rigidbody2D>();
-
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -25,6 +28,21 @@ public class PlayerMovement : MonoBehaviour
             player.velocity = new Vector2(player.velocity.x, 14);
         }
 
+        if (player.velocity.y < 0)
+        {
+            spriteRenderer.sprite = spriteArray[2];
+        }
+        else
+        {
+            if (player.velocity.x >= 0)
+            {
+                spriteRenderer.sprite = spriteArray[0];
+            }
+            if (player.velocity.x < 0)
+            {
+                spriteRenderer.sprite = spriteArray[1];
+            }
+        }
      
     }
 
@@ -33,7 +51,14 @@ public class PlayerMovement : MonoBehaviour
         if (other.tag == "VOID")
         {
             // Starting position of player character
-            player.position = new Vector2(-13,-2);
-        }    
+            player.position = new Vector2(-13, -2);
+        }
+
+        // Once the friends are free, door will be set with
+        // an active tag "Exit" and this will trigger the next scene
+        if (other.tag == "Exit")
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 }
