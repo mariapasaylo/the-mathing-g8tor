cad1a148d5cc61f4d8b3ca03264144e253a124e5
Maria Pasaylo <90634830+mariapasaylo@users.noreply.github.com>
2024-04-19 19:16:35 -0500

    Added counter for energy cell
    Added collectible items and increases the counter displayed on the screen. Changed the font as well

--- the mathing g8tor/Assets/Scripts/ItemCollector.cs
+++ the mathing g8tor/Assets/Scripts/ItemCollector.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class ItemCollector : MonoBehaviour
 {
     private int batteries = 0;
+    [SerializeField] private TextMeshProUGUI energyCellText;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Battery")) {
             Destroy(collision.gameObject);
             batteries++;
             Debug.Log("Batteries: " +  batteries);
+            energyCellText.text = "Energy Cell Count: " + batteries;
         }
     }
 }
