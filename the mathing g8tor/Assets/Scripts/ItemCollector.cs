2a2ffcf17a94575d0dcbad4df9d320374b4ad1ca
Maria Pasaylo <90634830+mariapasaylo@users.noreply.github.com>
2024-04-21 22:56:49 -0400

    Associated batteries with corresponding energy cell value

--- the mathing g8tor/Assets/Scripts/ItemCollector.cs
+++ the mathing g8tor/Assets/Scripts/ItemCollector.cs
@@ -7,15 +7,52 @@ using TMPro;
 
 public class ItemCollector : MonoBehaviour
 {
-    private int batteries = 0;
+    private int currentTotalEnergy = 0;
     [SerializeField] private TextMeshProUGUI energyCellText;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Battery")) {
-            Destroy(collision.gameObject);
-            batteries++;
-            Debug.Log("Batteries: " +  batteries);
-            energyCellText.text = "Energy Cell Count: " + batteries;
+        var batteryValues = new Dictionary<string, int>() {
+            {"Battery1",1 },
+            {"Battery3",3 },
+            {"Battery5",5 },
+            {"Battery7",7 },
+            {"Battery9",9 }
+        };
+
+        foreach (var battery in batteryValues) {
+
+            if (collision.gameObject.CompareTag(battery.Key))
+            {
+                Destroy(collision.gameObject); //MARIA instead of destroy animate to shut flame off and relight after one friend goes to portal
+                currentTotalEnergy = currentTotalEnergy + batteryValues[battery.Key];
+                Debug.Log(batteryValues[battery.Key]);
+                energyCellText.text = "Energy Cell Count: " + currentTotalEnergy;
+            }
         }
+
+
     }
 }
+
+
+
+/*
+
+        if (collision.gameObject.CompareTag("Battery1")) {
+            Destroy(collision.gameObject); //MARIA instead of destroy animate to shut flame off and respawn after one friend goes to portal
+            currentTotalEnergy = currentTotalEnergy + 1;
+ //           Debug.Log("Batteries: " +  batteries);
+            energyCellText.text = "Energy Cell Count: " + currentTotalEnergy;
+        }
+
+        if (collision.gameObject.CompareTag("Battery3"))
+        {
+            Destroy(collision.gameObject);
+            currentTotalEnergy = currentTotalEnergy + 3;
+            energyCellText.text = "Energy Cell Count: " + currentTotalEnergy;
+        }
+
+
+ 
+ 
+ */
